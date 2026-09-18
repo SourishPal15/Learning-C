70b91adff291f466d1e935fd5c3fc6673e81aaf1
SourishPal15 <sourishpal2008@gmail.com>
2026-09-19 00:15:21 +0530

    Added new lessons

---
+++ C(1)-Basics/L9ternaryoperator.c
@@ -0,0 +1,33 @@
+// In this lesson we will learn about ternary operators
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Read the comments of this lesson.");
+
+    return 0;
+}
+
+/*
+
+Basically there is a short hand way to write the conditional statement, 
+that is by using ternary operators.
+syntax:
+
+condition ? expression-if-true : expression-if-false ;
+
+example:
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a,&b);
+
+    a>b?printf("%d greater",a):printf("%d greater",b);
+    return 0;
+}
+
+*/
