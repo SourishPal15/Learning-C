6ae9a0aab206c56e9caab6268735d4583753d9b0
SourishPal15 <sourishpal2008@gmail.com>
2026-09-22 17:56:51 +0530

    Added new stuff

---
+++ C(01)-Basics/L10_loops.c
@@ -0,0 +1,26 @@
+// This lesson is about Loop statements in C language
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Read the comments of this lesson");
+
+    return 0;
+}
+
+/*
+
+Loops in C are control flow structures used to execute a block of code
+repeatedly as long as a specified condition remains true, they reduce
+code redundancy, save lines of code, and make repetitive task easy
+
+Three types of loops in C: 
+
+1) For-loop
+2) While-loop
+3) Do-While-loop
+
+These loops are discussed in details in the "C(04)-Loops" folder.
+
+*/
