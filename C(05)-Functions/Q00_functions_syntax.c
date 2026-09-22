69c33ca7d01201c7cdd3a6d6ec5a9bd3acb78d4d
SourishPal15 <sourishpal2008@gmail.com>
2026-09-22 19:39:11 +0530

    Added new stuff

---
+++ C(05)-Functions/Q00_functions_syntax.c
@@ -0,0 +1,38 @@
+// This lesson contains the syntax of functions
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Read the comments of this lesson");
+
+    return 0;
+}
+
+/*
+
+We are taking an example of int function which returns an int value,
+use void in a function to not return anything 
+
+Syntax:
+
+#include <stdio.h>
+
+int [function_name](int);  // function prototype
+
+int [function_name](int x)  // function definiton 
+{
+    [code]
+    return [return value]; 
+}
+
+int main()
+{
+    [code]
+
+    [fucntion_name](value);  // function calling
+
+    return 0;
+}
+
+*/
