1d61696ad0a61bbe05cb7ead1a9734150c5bf20f
SourishPal15 <sourishpal2008@gmail.com>
2026-09-22 19:31:48 +0530

    Added new stuff

---
+++ C(01)-Basics/L11_functions.c
@@ -0,0 +1,39 @@
+// This lesson teaches about functions and its usage in C language
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Read the comments of this lesson");
+
+    return 0;
+}
+
+/*
+
+A function is a block of code which performs a particlar task. A function
+can be reused inside the program by the programmer any number of times
+
+We use functions to: 
+
+1) To avoid using the same logic repeatedly
+2) For better readability in our program
+3) To test and check logic independently
+
+Types of Functions: 
+
+1) Libreary funcions
+2) User-Defined functions
+
+Function prototype: It is a way to tell the compiler about the function that we 
+will define in the code
+
+Function definition: It is our function where it is defined and the code is written
+inside the function
+
+Function call: It instructs the compiler to execute the body/code of the function
+when we call the fucntion
+
+Function related programs are discussed in details in "C(05)-Functions" folder
+
+*/
